24a6e6013391c91be01af2d2c04478fea883ada0
You Huang <youhuang0607@gmail.com>
2024-10-03 18:35:11 +0800

    update

--- tools/dist_test_no_viz.sh
+++ tools/dist_test_no_viz.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+# Check if the first argument is a port number
 if [[ $1 =~ ^[0-9]+$ ]]; then
     PORT=$1
     CONFIG=$2
+    CHECKPOINT=$3
+    GPUS=$4
+    shift 4
+else
+    PORT=29400
+    CONFIG=$1
+    CHECKPOINT=$2
     GPUS=$3
     shift 3
-else
-    PORT=29500
-    CONFIG=$1
-    GPUS=$2
-    shift 2
 fi
 
 NNODES=${NNODES:-1}
@@ -23,6 +26,7 @@ python -m torch.distributed.launch \
     --master_addr=$MASTER_ADDR \
     --nproc_per_node=$GPUS \
     --master_port=$PORT \
-    $(dirname "$0")/train.py \
+    $(dirname "$0")/test_no_viz.py \
     $CONFIG \
+    $CHECKPOINT \
     --launcher pytorch "$@"
--- tools/train.py
+++ tools/train.py
@@ -1,50 +1,30 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import argparse
+import logging
 import os
 import os.path as osp
-from copy import deepcopy
 
-from mmengine.config import Config, ConfigDict, DictAction
-from mmengine.registry import RUNNERS
+from mmengine.config import Config, DictAction
+from mmengine.logging import print_log
 from mmengine.runner import Runner
-from mmengine.utils import digit_version
-from mmengine.utils.dl_utils import TORCH_VERSION
+
+from mmseg.registry import RUNNERS
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Train a model')
+    parser = argparse.ArgumentParser(description='Train a segmentor')
     parser.add_argument('config', help='train config file path')
     parser.add_argument('--work-dir', help='the dir to save logs and models')
     parser.add_argument(
         '--resume',
-        nargs='?',
-        type=str,
-        const='auto',
-        help='If specify checkpoint path, resume from it, while if not '
-        'specify, try to auto resume from the latest checkpoint '
-        'in the work directory.')
+        action='store_true',
+        default=False,
+        help='resume from the latest checkpoint in the work_dir automatically')
     parser.add_argument(
         '--amp',
         action='store_true',
+        default=False,
         help='enable automatic-mixed-precision training')
-    parser.add_argument(
-        '--no-validate',
-        action='store_true',
-        help='whether not to evaluate the checkpoint during training')
-    parser.add_argument(
-        '--auto-scale-lr',
-        action='store_true',
-        help='whether to auto scale the learning rate according to the '
-        'actual batch size and the original batch size.')
-    parser.add_argument(
-        '--no-pin-memory',
-        action='store_true',
-        help='whether to disable the pin_memory option in dataloaders.')
-    parser.add_argument(
-        '--no-persistent-workers',
-        action='store_true',
-        help='whether to disable the persistent_workers option in dataloaders.'
-    )
     parser.add_argument(
         '--cfg-options',
         nargs='+',
@@ -71,14 +51,14 @@ def parse_args():
     return args
 
 
-def merge_args(cfg, args):
-    """Merge CLI arguments to config."""
-    if args.no_validate:
-        cfg.val_cfg = None
-        cfg.val_dataloader = None
-        cfg.val_evaluator = None
+def main():
+    args = parse_args()
 
+    # load config
+    cfg = Config.fromfile(args.config)
     cfg.launcher = args.launcher
+    if args.cfg_options is not None:
+        cfg.merge_from_dict(args.cfg_options)
 
     # work_dir is determined in this priority: CLI > segment in file > filename
     if args.work_dir is not None:
@@ -91,65 +71,26 @@ def merge_args(cfg, args):
                                             './configs'),
                                 osp.splitext(osp.basename(args.config))[0])
 
+    # make work_dir in model's train config
+    cfg['model']['train_cfg'].update(dict(work_dir=cfg.work_dir))
+
     # enable automatic-mixed-precision training
     if args.amp is True:
-        optim_wrapper = cfg.optim_wrapper.get('type', 'OptimWrapper')
-        assert optim_wrapper in ['OptimWrapper', 'AmpOptimWrapper'], \
-            '`--amp` is not supported custom optimizer wrapper type ' \
-            f'`{optim_wrapper}.'
-        cfg.optim_wrapper.type = 'AmpOptimWrapper'
-        cfg.optim_wrapper.setdefault('loss_scale', 'dynamic')
+        optim_wrapper = cfg.optim_wrapper.type
+        if optim_wrapper == 'AmpOptimWrapper':
+            print_log(
+                'AMP training is already enabled in your config.',
+                logger='current',
+                level=logging.WARNING)
+        else:
+            assert optim_wrapper == 'OptimWrapper', (
+                '`--amp` is only supported when the optimizer wrapper type is '
+                f'`OptimWrapper` but got {optim_wrapper}.')
+            cfg.optim_wrapper.type = 'AmpOptimWrapper'
+            cfg.optim_wrapper.loss_scale = 'dynamic'
 
     # resume training
-    if args.resume == 'auto':
-        cfg.resume = True
-        cfg.load_from = None
-    elif args.resume is not None:
-        cfg.resume = True
-        cfg.load_from = args.resume
-
-    # enable auto scale learning rate
-    if args.auto_scale_lr:
-        cfg.auto_scale_lr.enable = True
-
-    # set dataloader args
-    default_dataloader_cfg = ConfigDict(
-        pin_memory=True,
-        persistent_workers=True,
-        collate_fn=dict(type='default_collate'),
-    )
-    if digit_version(TORCH_VERSION) < digit_version('1.8.0'):
-        default_dataloader_cfg.persistent_workers = False
-
-    def set_default_dataloader_cfg(cfg, field):
-        if cfg.get(field, None) is None:
-            return
-        dataloader_cfg = deepcopy(default_dataloader_cfg)
-        dataloader_cfg.update(cfg[field])
-        cfg[field] = dataloader_cfg
-        if args.no_pin_memory:
-            cfg[field]['pin_memory'] = False
-        if args.no_persistent_workers:
-            cfg[field]['persistent_workers'] = False
-
-    set_default_dataloader_cfg(cfg, 'train_dataloader')
-    set_default_dataloader_cfg(cfg, 'val_dataloader')
-    set_default_dataloader_cfg(cfg, 'test_dataloader')
-
-    if args.cfg_options is not None:
-        cfg.merge_from_dict(args.cfg_options)
-
-    return cfg
-
-
-def main():
-    args = parse_args()
-
-    # load config
-    cfg = Config.fromfile(args.config)
-
-    # merge cli arguments to config
-    cfg = merge_args(cfg, args)
+    cfg.resume = args.resume
 
     # build the runner from config
     if 'runner_type' not in cfg:
