e5992abbe839897280ea9305760cf02cbf8fedd9
You Huang <youhuang0607@gmail.com>
2024-10-10 09:27:22 +0800

    main v1

---
+++ configs/datasets/train_simaug_gen_actions_v0_main_v1.py
@@ -0,0 +1,18 @@
+batch_size = 16
+train_dataloader = dict(
+    batch_size=batch_size,
+    num_workers=4,
+    persistent_workers=True,
+    sampler=dict(type='InfiniteSampler'),
+    dataset=dict(
+        type='GeneratedActionsV0Dataset',
+        pipeline=[
+            dict(type='GomokuAugmentation'),
+            dict(type='PackGomokuInputs',
+                 meta_keys=('history', 'action',
+                            'ori_history', 'ori_action',
+                            'homogenous_index',
+                            'left_step')),  # additional key
+        ], data_root='work_dirs/data_generation/main_v1'
+    )
+)
